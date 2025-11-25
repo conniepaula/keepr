75500ecdc8bb8565489a7c1461beda4e5b520322
Connie <conniedpaula@gmail.com>
2025-11-25 11:33:48 -0300

    chore: set up expo router

--- android/app/src/main/java/com/cpse/keepr/MainActivity.kt
+++ android/app/src/main/java/com/cpse/keepr/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.keepr
+package com.cpse.keepr
 
 import android.os.Build
 import android.os.Bundle
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text style={{ flex: 1, justifyContent: "center" }}> Hello Expo</Text>
+    </View>
+  );
+}
